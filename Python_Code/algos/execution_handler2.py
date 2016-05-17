f420885de4b349af1a16405d24507fc6de9aac79
Max <maxime.fages@gmail.com>
2016-05-17 21:42:39 +0800

    test on self.position existence. IB is weird

--- Python_Code/algos/execution_handler2.py
+++ Python_Code/algos/execution_handler2.py
@@ -453,13 +453,13 @@ class ExecutionHandler(object):
 
 
     def neutralize(self):
-
-        if self.position > 0 and self.position is not None:
-            neut = self.create_order("MKT",abs(self.position),"SELL")
-        if self.position < 0:
-            neut = self.create_order("MKT", abs(self.position), "BUY")
-        self.execute_order(neut)
-        time.sleep(1)
+        if  self.position is not None:
+            if self.position > 0:
+                neut = self.create_order("MKT",abs(self.position),"SELL")
+            if self.position < 0:
+                neut = self.create_order("MKT", abs(self.position), "BUY")
+            self.execute_order(neut)
+            time.sleep(1)
 
         self.ib_conn.reqGlobalCancel()
 
